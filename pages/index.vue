c203a4758d4442c92b076982fa7f514c2d65a520
Levi Whalen <levi@leviwhalen.com>
2019-10-07 15:45:45 -0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Nacelle Crewmember Assessment</h1>
+      <div class="columns">
+        <div
+          class="column is-4 product"
+          v-for="product in products"
+          :key="product.id"
+          @click="selectProduct"
+        >
+          <img :src="product.img" />
+          <h3>{{product.title}}</h3>
+          <p>{{product.price}}</p>
+        </div>
+      </div>
+      <div class="columns">
+        <h2>Selected Products</h2>
+        <!-- ADD A COMPONENT TO VIEW SELECTED PRODUCTS HERE -->
+      </div>
+      <div class="columns">
+        <h2>Total Value:</h2>
+        <!-- CALCULATE TOTAL VALUE OF SELECTED PRODUCTS -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import productData from "~/static/products.js";
+export default {
+  data() {
+    return {
+      products: productData.products
+    };
+  },
+  methods: {
+    selectProducts() {}
+  }
+};
+</script>
+<style lang="scss" scoped>
+h1 {
+  font-size: 24pt;
+  margin-bottom: 4rem;
+}
+.product {
+  padding: 2rem;
+  h3 {
+    font-size: 18pt;
+  }
+}
+</style>
---
+++ static/products.js
@@ -0,0 +1,12 @@
+export default {
+  products: [
+    { id: 1, title: "Uniform", price: 129.0, img: "/uniform.jpg" },
+    { id: 2, title: "Phaser", price: 499.0, img: "/phaser.jpg" },
+    {
+      id: 3,
+      title: "Dilithium Crystals",
+      price: 29.0,
+      img: "/dilithium-crystals.jpg"
+    }
+  ]
+};
