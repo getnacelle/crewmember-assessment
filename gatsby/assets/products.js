33445ab58eca7f0b79fdfa8cca51d6aa9d7f7a32
Nick Richmond <nwrichmond@turnt.tech>
2020-04-01 19:40:37 -0400

    Import product data

--- gatsby/assets/products.js
+++ gatsby/assets/products.js
@@ -1,16 +1,20 @@
+import uniform from './uniform.jpg';
+import phaser from './phaser.jpg';
+import dilithiumCrystals from './dilithium-crystals.jpg';
+
 export default {
   products: [
     {
-      id: 1, title: 'Uniform', price: 129.0, img: '/uniform.jpg', altText: 'A snazzy uniform'
+      id: 1, title: 'Uniform', price: 129.0, img: uniform, altText: 'A snazzy uniform'
     },
     {
-      id: 2, title: 'Phaser', price: 499.0, img: '/phaser.jpg', altText: 'A phaser set to stun'
+      id: 2, title: 'Phaser', price: 499.0, img: phaser, altText: 'A phaser set to stun'
     },
     {
       id: 3,
       title: 'Dilithium Crystals',
       price: 29.0,
-      img: '/dilithium-crystals.jpg',
+      img: dilithiumCrystals,
       altText: 'Glistening dilithium crystals'
     }
   ]
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React from 'react';
+import productData from 'assets/products';
+import './styles.scss';
 
-const Homepage = () => (
-  <>
-    <div class="container">
-      <h1>Nacelle Crewmember Assessment</h1>
-      <div class="columns">
-        <div class="column is-4 product">
-          <img src={product.img} />
-          <h3>{product.title}</h3>
-          <p>{product.price}</p>
+const { products } = productData;
+
+export default () => {
+  const productList = products.map(
+    // TURN THIS INTO A SEPARATE COMPONENT
+    (product) => (
+      <div className="column is-4 product" key={product.id}>
+        <img src={product.img} alt={product.altText} />
+        <h3>{product.title}</h3>
+        <p>{product.price}</p>
+      </div>
+    )
+  );
+  return (
+    <>
+      <div className="container">
+        <h1>Nacelle Crewmember Assessment</h1>
+        <div className="columns">
+          {productList}
+        </div>
+        <div className="columns">
+          <h2>Selected Products</h2>
+          {/* ADD A COMPONENT TO VIEW SELECTED PRODUCTS HERE */}
+        </div>
+        <div className="columns">
+          <h2>Total Value:</h2>
+          {/* CALCULATE TOTAL VALUE OF SELECTED PRODUCTS */}
         </div>
       </div>
-      <div class="columns">
-        <h2>Selected Products</h2>
-        {/* ADD A COMPONENT TO VIEW SELECTED PRODUCTS HERE */}
-      </div>
-      <div class="columns">
-        <h2>Total Value:</h2>
-        {/* CALCULATE TOTAL VALUE OF SELECTED PRODUCTS */}
-      </div>
-    </div>
-  </>
-)
-
-export default Homepage
+    </>
+  );
+};
