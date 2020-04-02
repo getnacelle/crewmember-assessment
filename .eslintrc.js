667904cf81b1d43a08290d31875100dea881a899
Nick Richmond <nwrichmond@turnt.tech>
2020-04-02 18:03:24 -0400

    Don't require keypress listeners for this exercise

--- .eslintrc.js
+++ .eslintrc.js
@@ -98,6 +98,8 @@ module.exports = {
     ],
     'jsx-a11y/accessible-emoji': 0,
     'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': [
       'warn',
       {
